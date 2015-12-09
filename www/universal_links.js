e000d7024b44160c0427c46df59845de5556033d
Robert Richter <robert@gimalon.com>
2015-12-09 16:25:13 +0100

    Listeners ready callback
    Listeners ready callback

--- www/universal_links.js
+++ www/universal_links.js
@@ -1,20 +1,13 @@
 var exec = require('cordova/exec'),
   channel = require('cordova/channel'),
 
-  // Reference name for the plugin
-  PLUGIN_NAME = 'UniversalLinks',
+// Reference name for the plugin
+PLUGIN_NAME = 'UniversalLinks',
 
-  // Plugin methods on the native side that can be called from JavaScript
-  pluginNativeMethod = {
-    INITIALIZE: 'jsInitPlugin'
-  };
-
-// Called when Cordova is ready for work.
-// Here we will send default callback to the native side through which it will send to us different events.
-channel.onCordovaReady.subscribe(function() {
-  ensureCustomEventExists();
-  exec(nativeCallback, null, PLUGIN_NAME, pluginNativeMethod.INITIALIZE, []);
-});
+// Plugin methods on the native side that can be called from JavaScript
+pluginNativeMethod = {
+  INITIALIZE: 'jsInitPlugin'
+};
 
 /**
  * Method is called when native side sends us events about application launch from the link click.
@@ -28,7 +21,7 @@ function nativeCallback(msg) {
   });
 
   document.dispatchEvent(ulEvent);
-}
+};
 
 /*
  * Polyfill for adding CustomEvent which may not exist on older versions of Android.
@@ -53,4 +46,26 @@ function ensureCustomEventExists() {
 
   CustomEvent.prototype = window.Event.prototype;
   window.CustomEvent = CustomEvent;
-}
+};
+
+if(!window.plugins) {
+  window.plugins = {};
+};
+
+if (!window.plugins.universalLink) {
+  window.plugins.universalLink = {initialized: false};
+};
+
+window.plugins.universalLink.listenersReady =
+  function() {
+    if( !window.plugins.universalLink.initialized ) {
+      console.log("universal_links plugin listeners ready");
+      ensureCustomEventExists();
+      exec(nativeCallback, null, PLUGIN_NAME, pluginNativeMethod.INITIALIZE, []);
+      window.plugins.universalLink.initialized = true;
+    }
+  };
+
+
+
+
